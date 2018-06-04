b64485f29768876145b28638d0ec2676b8e1bb6a
zzycat <can0106@live.com>
2018-06-04 15:41:14 +0800

    first time after merge

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -10,8 +10,9 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #app {
+  width: 100%;
   height: 100%;
 }
 </style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="login">
-    <el-form class="login-form" label-position="top" ref="form" :model="form" label-width="80px">
+  <div class="login-wrap">
+    <el-form
+      label-position="top"
+      :rules="rules"
+      ref="ruleForm"
+      :model="formData"
+      label-width="80px">
       <h2>用户登录</h2>
-      <el-form-item label="用户名">
-        <el-input v-model="form.username"></el-input>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="formData.username"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input v-model="form.password"></el-input>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="formData.password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button class="login-btn" type="primary">登 录</el-button>
+        <el-button class="login-btn" type="primary" @click="login">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -19,30 +24,69 @@
 export default {
   data() {
     return {
-      form: {
+      formData: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 11, message: '长度在 6 到 11 个字符', trigger: 'blur' }
+        ]
       }
     };
+  },
+  methods: {
+    async login() {
+      // 表单验证
+      this.$refs.ruleForm.validate(async (valid) => {
+        if (!valid) {
+          return;
+        }
+        const res = await this.$http.post('/login', this.formData);
+        const data = res.data;
+        if (data.meta.status === 200) {
+          this.$message({
+            type: 'success',
+            message: '登录成功!'
+          });
+          // 登录成功，把token记录到本地存储中
+          localStorage.setItem('token', data.data.token);
+          this.$router.push({
+            name: 'home'
+          });
+        } else {
+          // 登录失败，返回失败的原因
+          this.$message({
+            type: 'error',
+            message: data.meta.msg
+          });
+        }
+      });
+    }
   }
 };
 </script>
 
 <style scoped>
-  .login {
+  .login-wrap {
     background-color: #324152;
     height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
   }
-  .login .login-form {
-    background-color: #fff;
-    width: 400px;
-    padding: 30px;
+  .el-form.el-form--label-top {
+    padding: 40px;
+    width: 500px;
     border-radius: 5px;
+    background-color: #fff;
   }
-  .login .login-btn {
+  .el-form .login-btn {
     width: 100%;
   }
 </style>
